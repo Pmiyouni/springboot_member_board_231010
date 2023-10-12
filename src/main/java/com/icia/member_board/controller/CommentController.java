df9afd8717ab584e0f06bab505d060d6fc48a5cb
Pmiyouni <joeun125@naver.com>
2023-10-12 12:10:47 +0900

    댓글 삭제 추가

--- src/main/java/com/icia/member_board/controller/CommentController.java
+++ src/main/java/com/icia/member_board/controller/CommentController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -31,4 +29,12 @@ public class CommentController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        commentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/icia/member_board/service/CommentService.java
+++ src/main/java/com/icia/member_board/service/CommentService.java
@@ -47,4 +47,8 @@ public class CommentService {
         });
         return commentDTOList;
     }
+
+    public void delete(Long id) {
+        commentRepository.deleteById(id);
+    }
 }
